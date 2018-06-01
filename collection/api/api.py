0582e5fc339c49682899fbe4060b1082366711c4
kickscar <kickscar@gmail.com>
2018-06-01 11:20:14 +0900

    pd_gen_url pd_fetch_tourspot_visitor 작성

--- collection/api/api.py
+++ collection/api/api.py
@@ -1,18 +1,40 @@
+from urllib.parse import urlencode
 from .json_request import json_request
 
 
-def pd_gen_url(endpoint, service_key, **params):
-    pass
+def pd_gen_url(
+        endpoint,
+        service_key='xndt9aE0vWNU2zQcWp8iBPms%2BTgWggyBYGwQkxs47RHcSdy12U%2FpUDZ7dS4TT33OLafiai%2By6fiCNqdkEwnkWA%3D%3D',
+        # service_key='%2FfZdR%2Bue1CSxLEnMkZXa9iDYontLTMTIteD5%2BzYCiMYpDKUZNUh2FHGDQ04zazSEmLl34FClDQk8a7flFCIQKA%3D%3D',
+        **params):
+    return '%s?serviceKey=%s&%s' % (endpoint, service_key, urlencode(params))
 
 
 def pd_fetch_foreign_visitor(country_code=0, year=0, month=0, service_key=''):
     pass
+
 
 def pd_fetch_tourspot_visitor(
         district1='',
         district2='',
         tourspot='',
         year=0,
-        month=0,
-        service_key=''):
-    pass
+        month=0):
+    gen_url = pd_gen_url(
+        endpoint='http://openapi.tour.go.kr/openapi/service/TourismResourceStatsService/getPchrgTrrsrtVisitorList',
+        YM='{0:04d}{1:02d}'.format(year, month),
+        SIDO=district1,
+        GUNGU=district2,
+        RES_NM=tourspot,
+        _type='json')
+    json_response = json_request(gen_url)
+    result_msg = json_response\
+        .get('response')\
+        .get('header')\
+        .get('resultMsg')
+
+    return json_response\
+        .get('response')\
+        .get('body')\
+        .get('items')\
+        .get('item') if result_msg == 'OK' else []
--- collection/crawler.py
+++ collection/crawler.py
@@ -1,4 +1,4 @@
-
+from collection import api
 
 def preprocess_tourspot_visitor(item):
     pass
@@ -8,21 +8,11 @@ def preprocess_foreign_visitor(data):
     pass
 
 
-def crawling_foreign_visitor(
-        country,
-        start_year,
-        end_year,
-        restore_directory,
-        service_key,
-        fetch=True):
+def crawling_foreign_visitor(country, start_year, end_year):
     pass
 
 
-def crawling_tourspot_visitor(
-        district,
-        start_year,
-        end_year,
-        restore_directory,
-        service_key,
-        fetch=True):
-    pass
+def crawling_tourspot_visitor(district, start_year, end_year):
+    for year in range(start_year, end_year+1):
+        for month in range(1, 13):
+            api.pd_fetch_tourspot_visitor(district1=district, year=year, month=month)
